1f21f83d4599b44e23bd72c443b12905bb0cb87f
Evan Lucas <evanlucas@me.com>
2015-06-03 10:40:28 -0500

    add raw option to not pretty format from cli
    also added a version flag and updated usage

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,21 +3,45 @@
 var help = require('help')()
   , remote = require('../')
   , pb = require('pretty-bytes')
-  , args = process.argv.splice(2)
+  , nopt = require('noptd')
+  , knownOpts = { raw: Boolean
+                , help: Boolean
+                , version: Boolean
+                }
+  , shortHand = { r: ['--raw']
+                , h: ['--help']
+                , v: ['--version']
+                }
+  , defs = { raw: false }
+  , parsed = nopt(knownOpts, shortHand)(defs)
+  , package = require('../package')
+
+if (parsed.help) {
+  return help(0)
+}
+
+if (parsed.version) {
+  console.log('remote-file-size', 'v' + package.version)
+  return
+}
+
+var args = parsed.argv.remain
 
 if (!args.length)
   return help(1)
 
-var h = ['help', '--help', '-h']
 var url = args.shift()
-if (~h.indexOf(url)) {
+if (url === 'help')
   return help(0)
-} else {
-  remote(url, function(err, out) {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
+
+remote(url, function(err, out) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  if (parsed.raw) {
+    console.log(out)
+  } else {
     console.log(pb(out))
-  })
-}
+  }
+})
