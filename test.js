2e839529c520cd29b35f91faad13a34a477e1ede
Evan Lucas <evanlucas@me.com>
2015-11-02 06:24:57 -0600

    test: add test for missing content-length

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 const test = require('tap').test
     , remote = require('./')
+    , http = require('http')
 
 test('should fail on invalid url', function(t) {
   remote('blah', function(err, out) {
@@ -39,5 +40,25 @@ test('should work passing an object', function(t) {
     t.ok(out, 'out should exist')
     t.equal(out, 1548)
     t.end()
+  })
+})
+
+test('should return error if invalid content-length', function(t) {
+  t.plan(2)
+  const server = http.createServer(function(req, res) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    })
+    res.end()
+  }).listen(0, function() {
+    const port = server.address().port
+    remote(`http://localhost:${port}/`, function(err, out) {
+      t.ok(err, 'err should exist')
+      t.equal(err.message, 'Unable to determine file size')
+    })
+  })
+
+  t.on('end', () => {
+    server.close()
   })
 })
